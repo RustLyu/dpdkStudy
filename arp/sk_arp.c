41359ae0cf54c50f921605f6d81288083032ed3c
sk.lyu <lvshuiku@126.com>
2023-07-11 09:52:40 +0000

    update

--- arp/sk_arp.c
+++ arp/sk_arp.c
@@ -4,6 +4,8 @@
 #include <arpa/inet.h>
 #include <rte_ether.h>
 #include <rte_arp.h>
+#include <inttypes.h>
+#include <rte_log.h>
 
 #define NUM_MBUFS (4096 - 1)
 #define BURST_SIZE 32
@@ -15,6 +17,8 @@
 #define N_TX_RING_SIZE 128
 
 #define  MBUF_CACHE_SIZE 250
+
+#define LOCAL_MAC ("00:0c:29:e4:9c:99")
 
 static const struct rte_eth_conf port_conf_default={
     .rxmode = {
@@ -52,7 +56,7 @@ int init_eth_port(const int port_id, struct rte_mempool* mpool)
     }
     return 0;
 }
-#define LOCAL_MAC ("")
+
 int create_eth_arp_pkt(uint8_t* msg, uint8_t* d_mac, uint32_t sip, uint32_t dip)
 {
     struct rte_ether_hdr* eth_hdr = (struct rte_ether_hdr*) msg;
@@ -106,8 +110,17 @@ int send_arp(struct rte_mempool* mbuf_pool, uint8_t* d_mac, uint32_t sip, uint32
 
 void print_ip_address(uint32_t ip) {
     uint8_t *byte = (uint8_t *)&ip;
-    printf("%d.%d.%d.%d\n", byte[3], byte[2], byte[1], byte[0]);
+    printf("%d.%d.%d.%d\n", byte[0], byte[1], byte[2], byte[3]);
 }
+
+void set_eth_hdr(struct rte_ether_hdr* hdr)
+{}
+
+void set_ipv4_hdr(struct rte_ipv4_hdr* hdr)
+{}
+
+void set_icmp_hdr(struct rte_icmp_hdr* hdr)
+{}
 
 int main(int argc, char** argv)
 {
@@ -118,7 +131,7 @@ int main(int argc, char** argv)
     {
         rte_exit(EXIT_FAILURE, "failed init eal dpdk");
     }
-    
+    rte_log_set_level(RTE_LOG_DEBUG, 0); 
     // find cpu core
     uint16_t port_id = rte_eth_find_next_owned_by(0, RTE_ETH_DEV_NO_OWNER);
     if(port_id == RTE_MAX_ETHPORTS)
@@ -135,25 +148,30 @@ int main(int argc, char** argv)
 
     // config port
     ret = init_eth_port(port_id, mbuf_pool);
+
+    // start rx/tx data
     if(ret  >= 0)
     {
         while(1)
         {      
             struct rte_mbuf* buf[BURST_SIZE];
+            // struct rte_mbuf* tx_buf[BURST_SIZE];
             unsigned n_recv = rte_eth_rx_burst(port_id, 0, buf, BURST_SIZE);
             if(n_recv > 0)
             {
-                printf("recv msg:%d\n", n_recv);
+                //printf("recv msg:%d\n", n_recv);
             }
             for(int i = 0; i < n_recv; ++i)
             {
                 struct rte_ether_hdr* eth_hdr = rte_pktmbuf_mtod(buf[i], struct rte_ether_hdr*);
+
+                /// ARP
                 if(eth_hdr->ether_type == rte_cpu_to_be_16(RTE_ETHER_TYPE_ARP))
                 {
-                    printf("recv arp command");
+                    //printf("recv arp command");
                     struct rte_arp_hdr* arp_hdr = (struct rte_arp_hdr*)(eth_hdr+1);
 
-                    print_ip_address(arp_hdr->arp_data.arp_tip);
+                    //print_ip_address(arp_hdr->arp_data.arp_tip);
                 
                     if(arp_hdr->arp_data.arp_tip == "192.168.0.127")
                     {
@@ -161,7 +179,86 @@ int main(int argc, char** argv)
                     }
                     else
                     {
-                        printf("recv: ip do not equal%d\n", arp_hdr->arp_data.arp_tip);
+                        //printf("recv: ip do not equal%d\n", arp_hdr->arp_data.arp_tip);
+                    }
+                }
+                else
+                {
+                    struct rte_ipv4_hdr* ip_hdr = (struct rte_ipv4_hdr*)(eth_hdr + 1);
+                    /// ICMP
+                    if(ip_hdr->next_proto_id == IPPROTO_ICMP)
+                    {
+
+                        struct rte_icmp_hdr* icmp_hdr = (struct rte_icmp_hdr*)(ip_hdr + 1);
+
+                        struct rte_mbuf* tx_pkt = rte_pktmbuf_alloc(mbuf_pool);
+
+                        struct rte_ether_hdr* tx_eth_hdr = rte_pktmbuf_mtod(tx_pkt, struct rte_ether_hdr*);
+                        struct rte_ipv4_hdr* tx_ip_hdr = (struct rte_ipv4_hdr*)(tx_eth_hdr + 1);
+                        struct rte_icmp_hdr* tx_icmp_hdr = (struct rte_icmp_hdr*)(tx_ip_hdr + 1);
+                        //set_eth_hdr(eth_hdr);
+                        // build eth hdr
+                        // rte_memcpy(&tx_eth_hdr->src_addr.addr_bytes, LOCAL_MAC, RTE_ETHER_ADDR_LEN);
+                        rte_ether_addr_copy(&eth_hdr->src_addr.addr_bytes, &tx_eth_hdr->dst_addr.addr_bytes);
+                        rte_ether_addr_copy(&eth_hdr->dst_addr.addr_bytes, &tx_eth_hdr->src_addr.addr_bytes);
+                        tx_eth_hdr->ether_type = htons(RTE_ETHER_TYPE_IPV4);//eth_hdr->ether_type;
+                        
+                        // build ipv4 hdr
+                        tx_ip_hdr->version_ihl = 0x45; //(4 << 4) | (5 & 0xF); // version is ipv4, hdr len 5
+                        tx_ip_hdr->type_of_service = 0;
+                        tx_ip_hdr->total_length = rte_cpu_to_be_16(sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_icmp_hdr));
+                        tx_ip_hdr->packet_id = 0;//ip_hdr->packet_id;
+                        tx_ip_hdr->fragment_offset = 0;//rte_cpu_to_be_16(RTE_IPV4_HDR_DF_FLAG);
+                        tx_ip_hdr->time_to_live = 64;
+                        tx_ip_hdr->next_proto_id = IPPROTO_ICMP;
+                        tx_ip_hdr->hdr_checksum = 0;
+                        tx_ip_hdr->src_addr = ip_hdr->dst_addr;
+                        tx_ip_hdr->dst_addr = ip_hdr->src_addr;
+                        tx_ip_hdr->hdr_checksum = rte_ipv4_cksum(tx_ip_hdr);
+
+                        print_ip_address(tx_ip_hdr->src_addr);
+                        print_ip_address(tx_ip_hdr->dst_addr);
+                        
+                        char mac_str[RTE_ETHER_ADDR_FMT_SIZE];
+                        rte_ether_format_addr(mac_str, RTE_ETHER_ADDR_FMT_SIZE, &tx_eth_hdr->src_addr.addr_bytes);
+                        printf("sd src MAC address: %s\n", mac_str);
+                        
+                        memset(mac_str, RTE_ETHER_ADDR_FMT_SIZE, 0);
+                        rte_ether_format_addr(mac_str, RTE_ETHER_ADDR_FMT_SIZE, &tx_eth_hdr->dst_addr.addr_bytes);
+                        printf("sd dst MAC address: %s\n", mac_str);
+
+                        memset(mac_str, RTE_ETHER_ADDR_FMT_SIZE, 0);
+                        rte_ether_format_addr(mac_str, RTE_ETHER_ADDR_FMT_SIZE, &eth_hdr->dst_addr.addr_bytes);
+                        printf("rc dst MAC address: %s\n", mac_str);
+                        memset(mac_str, RTE_ETHER_ADDR_FMT_SIZE, 0);
+                        rte_ether_format_addr(mac_str, RTE_ETHER_ADDR_FMT_SIZE, &eth_hdr->src_addr.addr_bytes);
+                        printf("rc src MAC address: %s\n", mac_str);
+
+                        // set icmp hdr
+                        tx_icmp_hdr->icmp_type = RTE_IP_ICMP_ECHO_REPLY;
+                        tx_icmp_hdr->icmp_code = 0;
+                        tx_icmp_hdr->icmp_ident = icmp_hdr->icmp_ident;
+                        tx_icmp_hdr->icmp_seq_nb = icmp_hdr->icmp_seq_nb;
+                        tx_icmp_hdr->icmp_cksum = 0;
+                        tx_icmp_hdr->icmp_cksum = rte_raw_cksum(tx_icmp_hdr, sizeof(struct rte_icmp_hdr));
+                        //set_ipv4_hdr(ip_hdr);
+                        //set_icmp_hdr(icmp_hdr);
+                        //tx_buf[0] = tx_pkt;
+                        const unsigned total_length = sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_icmp_hdr);
+                        tx_pkt->pkt_len = total_length;
+                        tx_pkt->data_len = total_length;
+                        int n_tx = rte_eth_tx_burst(port_id, 0, &tx_pkt, 1);
+                        if(n_tx < 1)
+                        {
+                            rte_pktmbuf_free(tx_pkt);
+                        }
+                        else
+                        {
+                            
+                            uint64_t error_no = 0;//rte_eth_tx_burst_error(port_id, 0);
+                            printf("recv: icmp msg failed error no:\n");
+                        }
+                        printf("recv: icmp msg\n");
                     }
                 }
             
